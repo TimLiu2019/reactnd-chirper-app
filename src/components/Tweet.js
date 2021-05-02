71f13c9e338f5d96f51db170a976a45ad4a265a8
TimLiu2019 <qitingliu7@gmail.com>
2021-05-02 09:00:05 -0400

    tweet component

---
+++ src/components/Tweet.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { formatTweet } from "../utils/helpers";
+
+class Tweet extends Component {
+  render() {
+    console.log("tweet", this.props);
+    return <div className="tweet"></div>;
+  }
+}
+function mapStateToProps({ authedUser, users, tweets }, { id }) {
+  const tweet = tweets[id];
+  return {
+    authedUser,
+    tweet: formatTweet(tweet, users[tweet.author], authedUser)
+  };
+}
+export default connect(mapStateToProps)(Tweet);
