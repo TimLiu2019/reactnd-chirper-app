0b29d6fd28544477150f03b5937eb7dea3abaaa8
TimLiu2019 <qitingliu7@gmail.com>
2021-05-04 10:31:49 -0400

    router

--- README.md
+++ README.md
@@ -9,13 +9,14 @@
 1. clone the Project - git@github.com:udacity/reactnd-chirper-app.git
 2. install the dependencies - npm install
 
-## steps to npm
+## steps of npm
 
 1. npm i redux
 2. npm i react-redux
 3. npm i redux-thunk
 4. yarn add react-icons
 5. yarn add react-redux-loading
+6. yarn add react-router-dom
 
 ## License
 
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,6 +5,7 @@ import { TiArrowBackOutline } from "react-icons/ti";
 import { TiHeartOutline } from "react-icons/ti";
 import { TiHeartFullOutline } from "react-icons/ti";
 import handleToggleTweet from "../actions/tweets";
+import { Link, withRouter } from "react-router-dom";
 
 class Tweet extends Component {
   handleLike = e => {
@@ -21,6 +22,7 @@ class Tweet extends Component {
   };
   toParent = (e, id) => {
     e.preventDefault();
+    this.props.history.push(`/tweet/${id}`);
   };
   render() {
     const { tweet } = this.props;
@@ -40,7 +42,7 @@ class Tweet extends Component {
     } = tweet;
     console.log("tweet", this.props);
     return (
-      <div className="tweet">
+      <Link to={`/tweet/${id}`} className="tweet">
         <img src={avatar} alt={`Avatar of ${name}`} className="avatar" />
         <div className="tweet-info">
           <div>
@@ -69,7 +71,7 @@ class Tweet extends Component {
             <span>{likes !== 0 && likes}</span>
           </div>
         </div>
-      </div>
+      </Link>
     );
   }
 }
@@ -83,4 +85,4 @@ function mapStateToProps({ authedUser, users, tweets }, { id }) {
       : null
   };
 }
-export default connect(mapStateToProps)(Tweet);
+export default withRouter(connect(mapStateToProps)(Tweet));
